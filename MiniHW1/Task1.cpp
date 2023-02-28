794cafb2981147d7aca4a91e08b7e99ccc7d2211
Kiril <kikodimov2002@gmail.com>
2023-02-28 12:55:59 +0200

    Add project files.

---
+++ MiniHW1/Task1.cpp
@@ -0,0 +1,13 @@
+/*
+	A color has 3 components: red, green, and blue.
+	
+	Make a structure 'Color' that will hold the data of a primitive color (3 bits).
+	Example: RED -> 100 / GREEN -> 010 / BLUE -> 001.
+
+	Make a function that prints the name of a given Color.
+	
+	Make a function that takes a Color and returns the opposite Color.
+	
+	Make a function that fuses a Color with a second Color 
+	and a function that removes a Color from a Color.
+*/
---
+++ MiniHW1/Task2.cpp
@@ -0,0 +1,18 @@
+/*
+	A monthly calendar has 12 months.
+
+	Make a structure 'Calendar' that will have a month (enum)
+	and a year (pick a fitting type).
+	
+	Make a function that prints a calendar's month and year.
+	
+	Make functions that take a calendar and print the next and previous month and/or year
+	(Make use of the previous function).
+
+	Make functions that save and load a calendar to a text file in the format:
+	{year} {month}
+	where both 'year' and 'month' are integer values.
+	// Note: This hasn't been covered in class yet,
+	// but you can do some research about the library 'fstream'
+	// as it is what we will be using going forward :)
+*/
